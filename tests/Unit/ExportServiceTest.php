f36015c65afb97785a9c5e321d6c43212bb0a8ce
Rd27p <g.a.raka.darma.p.k@gmail.com>
2026-04-26 10:53:36 +0700

    feat: [GAR] penambahan test case

--- tests/Unit/ExportServiceTest.php
+++ tests/Unit/ExportServiceTest.php
@@ -19,7 +19,7 @@ class ExportServiceTest extends TestCase
             ->with(500, \Mockery::any())
             ->andReturnUsing(function ($size, $callback) {
                 // Simulate chunking with fake items
-                $fakeItem = (object)['id' => 1, 'name' => 'Test'];
+                $fakeItem = (object) ['id' => 1, 'name' => 'Test'];
                 $callback(collect([$fakeItem]));
                 return true;
             });
@@ -43,5 +43,93 @@ class ExportServiceTest extends TestCase
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
             $response->headers->get('Content-Type')
         );
+    }
+
+    public function test_export_writes_headers_first()
+    {
+        $service = new ExportService();
+
+        $headers = ['ID', 'Name'];
+
+        $query = \Mockery::mock(Builder::class);
+        $query->shouldReceive('chunk')
+            ->once()
+            ->andReturnUsing(function ($size, $callback) {
+                $callback(collect([]));
+            });
+
+        $service->exportToExcel('test.xlsx', $headers, $query, function () {
+            return [];
+        });
+
+        $this->assertTrue(true); // minimal execution validation
+    }
+
+    public function test_export_handles_multiple_chunks()
+    {
+        $service = new ExportService();
+
+        $query = \Mockery::mock(Builder::class);
+
+        $query->shouldReceive('chunk')
+            ->with(500, \Mockery::any())
+            ->andReturnUsing(function ($size, $callback) {
+                $callback(collect([
+                    (object) ['id' => 1, 'name' => 'A'],
+                    (object) ['id' => 2, 'name' => 'B'],
+                ]));
+                return true;
+            });
+
+        $service->exportToExcel('test.xlsx', ['ID', 'Name'], $query, function ($item) {
+            return [$item->id, $item->name];
+        });
+
+        $this->assertTrue(true);
+    }
+
+    public function test_maprow_function_is_called_correctly()
+    {
+        $service = new ExportService();
+
+        $query = \Mockery::mock(Builder::class);
+
+        $query->shouldReceive('chunk')
+            ->once()
+            ->andReturnUsing(function ($size, $callback) {
+                $callback(collect([
+                    (object) ['id' => 99, 'name' => 'Raka'],
+                ]));
+                return true;
+            });
+
+        $service->exportToExcel('test.xlsx', ['ID', 'Name'], $query, function ($item) {
+            $this->assertEquals(99, $item->id);
+            $this->assertEquals('Raka', $item->name);
+
+            return [$item->id, strtoupper($item->name)];
+        });
+
+        $this->assertTrue(true);
+    }
+
+    public function test_export_handles_empty_dataset()
+    {
+        $service = new ExportService();
+
+        $query = \Mockery::mock(Builder::class);
+
+        $query->shouldReceive('chunk')
+            ->once()
+            ->andReturnUsing(function ($size, $callback) {
+                $callback(collect([]));
+                return true;
+            });
+
+        $response = $service->exportToExcel('test.xlsx', ['ID'], $query, function ($item) {
+            return [$item->id ?? null];
+        });
+
+        $this->assertInstanceOf(StreamedResponse::class, $response);
     }
 }
